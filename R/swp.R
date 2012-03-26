8528488deaed0033f97bd56d075fb011267faf54
Sacha Epskamp <sacha.epskamp@gmail.com>
2012-03-27 01:02:21 +0200

    0.0.3

--- R/swp.R
+++ R/swp.R
@@ -1,24 +1,5 @@
-##' Return statistic and p values in LaTeX format
-##' 
-##' This is the function used by \code{\link{swst}} to generate the LaTeX code.
-##' 
-##' 
-##' @param statistic Name of the statistic used
-##' @param value Value of the statistic
-##' @param pval p-value
-##' @param df The degrees of freedom. Can be a vector with multiple degrees of
-##' freedom
-##' @param digits The number of digits to be printed
-##' @param sig Logical, if TRUE not the real value but only the value compared
-##' to levels of 'alpha' is printed. This means that if p is smaller than one of
-##' the levels of 'alpha' than that is returned (e.g. p<0.05) else ns is
-##' returned.
-##' @param alpha A vector specifying the significance levels used by 'sig'
-##' @param parantheses Logical, should parantheses be printed?
-##' @author Sacha Epskamp
-##' @seealso \code{\link{swst}} \code{\link{swstNP}}
-##' @export
-swp <- function(statistic,value,pval,df=NULL,digits=3,sig=FALSE,alpha=c(0.05,0.01,0.001),parantheses=TRUE)
+
+swp <- function(statistic,value,pval,df=NULL,digits=3,sig=FALSE,alpha=c(0.05,0.01,0.001),output=c("Sweave","knitr"),parantheses=TRUE)
 {
   po <- ifelse(parantheses,"(","")
   pc <- ifelse(parantheses,")","")  
@@ -32,7 +13,7 @@ swp <- function(statistic,value,pval,df=NULL,digits=3,sig=FALSE,alpha=c(0.05,0.0
         statistic,
         "",
         ifelse(is.null(df),"",paste("(",paste(format(round(df,digits),scientific=FALSE),collapse=","),")",sep="")),
-        "=",
+        " = ",
         format(round(value,digits),scientific=FALSE),
         "$, ns",pc,sep="")
     } else
@@ -43,7 +24,7 @@ swp <- function(statistic,value,pval,df=NULL,digits=3,sig=FALSE,alpha=c(0.05,0.0
         statistic,
         "",
         ifelse(is.null(df),"",paste("(",paste(format(round(df,digits),scientific=FALSE),collapse=","),")",sep="")),
-        "=",
+        " = ",
         format(round(value,digits),scientific=FALSE),
         "$, $p<",
         format(alpha[whichsig],scientific=FALSE),
@@ -58,7 +39,7 @@ swp <- function(statistic,value,pval,df=NULL,digits=3,sig=FALSE,alpha=c(0.05,0.0
         statistic,
         "",
         ifelse(is.null(df),"",paste("(",paste(format(round(df,digits),scientific=FALSE),collapse=","),")",sep="")),
-        "=",
+        " = ",
         format(round(value,digits),scientific=FALSE),
         "$, $p=",
         format(round(pval,digits),scientific=FALSE),
@@ -70,12 +51,16 @@ swp <- function(statistic,value,pval,df=NULL,digits=3,sig=FALSE,alpha=c(0.05,0.0
         statistic,
         "",
         ifelse(is.null(df),"",paste("(",paste(format(round(df,digits),scientific=FALSE),collapse=","),")",sep="")),
-        "=",
+        " = ",
         format(round(value,digits),scientific=FALSE),
         "$, $p<",
         format(round(10^(-1*digits),digits),scientific=FALSE),
         "$",pc,sep="")    
     }
+  }
+  if (all(output=="plain"))
+  {
+    res <- gsub("\\$","",res)
   }
   return(res)  
 }
